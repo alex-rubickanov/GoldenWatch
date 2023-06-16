fce1c12f42e587f701a48ef2ae7c506a89aaca71
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-06-17 02:00:01 +0400

    Fixed Issue with Joining Controllers

--- GoldenWatchProject/Assets/Vince/Scripts/ControllerDetect.cs
+++ GoldenWatchProject/Assets/Vince/Scripts/ControllerDetect.cs
@@ -5,50 +5,63 @@ using UnityEngine;
 
 public class ControllerDetect : MonoBehaviour
 {
-    [SerializeField] string[] controllerNames;
+    [SerializeField] List<string> controllerNames;
     [SerializeField] GameObject[] playerReady;
     [SerializeField] GameObject[] joinBtns;
     [SerializeField] GameObject startText;
-    [SerializeField] float numOfPlayersReady;
-    void Start()
-    {
-        controllerNames = Input.GetJoystickNames();
-    }
+    [SerializeField] GameObject mainMenu;
+    int numOfPlayersReady;
 
-    // Update is called once per frame
     void Update()
     {
+        controllerNames = new List<string>(Input.GetJoystickNames());
+        RemoveEmptyControllerNames();
         DetectPlayerGamePads();
         StartGame();
     }
 
     private void StartGame()
     {
-        if(numOfPlayersReady > 1)
+        if (numOfPlayersReady > 1)
         {
             startText.SetActive(true);
-            //Load scene
+            // Load scene or start the game
         }
     }
 
     private void DetectPlayerGamePads()
     {
-        if (controllerNames[1] != null)
+        if (controllerNames.Count > 0 && !string.IsNullOrEmpty(controllerNames[0]))
         {
-            if (Input.GetKeyDown(KeyCode.JoystickButton0))
+            if (Input.GetKey(KeyCode.Joystick1Button0) && !mainMenu.activeSelf)
             {
+                SetNumberOfPlayerReady(0);
                 playerReady[0].SetActive(true);
                 joinBtns[0].SetActive(false);
             }
         }
 
-        if (controllerNames[2] != null)
+        if (controllerNames.Count > 1 && !string.IsNullOrEmpty(controllerNames[1]))
         {
-            if (Input.GetKeyDown(KeyCode.JoystickButton0))
+            if (Input.GetKey(KeyCode.Joystick2Button0) && !mainMenu.activeSelf)
             {
+                SetNumberOfPlayerReady(1);
                 playerReady[1].SetActive(true);
                 joinBtns[1].SetActive(false);
             }
         }
+    }
+
+    void SetNumberOfPlayerReady(int playerNum)
+    {
+        if (!playerReady[playerNum].activeSelf)
+        {
+            numOfPlayersReady++;
+        }
+    }
+
+    void RemoveEmptyControllerNames()
+    {
+        controllerNames.RemoveAll(name => string.IsNullOrEmpty(name));
     }
 }
