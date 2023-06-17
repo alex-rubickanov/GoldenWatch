7d362369ccab40bdcfeb8b08c80f29415856d506
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-06-17 04:13:39 +0400

    Added music

--- GoldenWatchProject/Assets/Alex/Scripts/Shotgun_Script.cs
+++ GoldenWatchProject/Assets/Alex/Scripts/Shotgun_Script.cs
@@ -13,19 +13,36 @@ public class Shotgun_Script : Weapon
     }
     public override void Shoot()
     {
-        if (timer >= shootingSpeed && currentAmmo > 0) {
-            timer = 0f;
-            for(int i = 0; i < bulletSpawners.Length; i++) {
-                GameObject bullet = Instantiate(bulletPrefab, bulletSpawners[i].transform.position, bulletSpawners[i].transform.rotation);
-                bullet.GetComponent<BulletDamage>().SetDamage(damage);
-                bullet.GetComponent<Rigidbody>().AddForce(bulletSpawners[i].transform.forward * bulletSpeed, ForceMode.Impulse);
+        if(gameObject.GetComponentInParent<ShootingSpeedX2>() != null) {
+            if (timer >= shootingSpeedX2 && currentAmmo > 0) {
+                timer = 0f;
+                for (int i = 0; i < bulletSpawners.Length; i++) {
+                    GameObject bullet = Instantiate(bulletPrefab, bulletSpawners[i].transform.position, bulletSpawners[i].transform.rotation);
+                    bullet.GetComponent<BulletDamage>().SetDamage(damage);
+                    bullet.GetComponent<Rigidbody>().AddForce(bulletSpawners[i].transform.forward * bulletSpeed, ForceMode.Impulse);
+                }
+                currentAmmo -= 1;
+                ParticleSystem gunShotFX = Instantiate(gunShotParticle, bulletSpawner.position, bulletSpawner.rotation);
+                var main = gunShotFX.main;
+                main.simulationSpeed = gunShotPlayBackSpeed;
+                audioManager.PlayGunSound(this, volume);
             }
-            currentAmmo -= 1;
-            ParticleSystem gunShotFX = Instantiate(gunShotParticle, bulletSpawner.position, bulletSpawner.rotation);
-            var main = gunShotFX.main;
-            main.simulationSpeed = gunShotPlayBackSpeed;
-           audioManager.PlayGunSound(this, volume);
+        } else {
+            if (timer >= shootingSpeed && currentAmmo > 0) {
+                timer = 0f;
+                for (int i = 0; i < bulletSpawners.Length; i++) {
+                    GameObject bullet = Instantiate(bulletPrefab, bulletSpawners[i].transform.position, bulletSpawners[i].transform.rotation);
+                    bullet.GetComponent<BulletDamage>().SetDamage(damage);
+                    bullet.GetComponent<Rigidbody>().AddForce(bulletSpawners[i].transform.forward * bulletSpeed, ForceMode.Impulse);
+                }
+                currentAmmo -= 1;
+                ParticleSystem gunShotFX = Instantiate(gunShotParticle, bulletSpawner.position, bulletSpawner.rotation);
+                var main = gunShotFX.main;
+                main.simulationSpeed = gunShotPlayBackSpeed;
+                audioManager.PlayGunSound(this, volume);
 
+            }
         }
+        
     }
 }
